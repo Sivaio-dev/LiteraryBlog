a40aba11e8a2ee754b8443e8b384987a6dd86952
Sivaio-dev <shivavijay0402@gmail.com>
2026-06-26 22:57:27 +0530

    Update CORS configuration to include new origin

--- literary-blog-backend/src/main/java/com/blog/literary_blog/config/WebConfig.java
+++ literary-blog-backend/src/main/java/com/blog/literary_blog/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedOrigins("https://kesavthoughts.vercel.app", "http://localhost:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
